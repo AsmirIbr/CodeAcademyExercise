a0934b18800b6bee2584dad3e2fd90b9f3405e92
Asmir <asmir.ibrovic@gmail.com>
2019-01-10 04:09:37 +0100

    ex

--- Frontend/JavaScript/MusicApp/entities/album.js
+++
@@ -1,5 +0,0 @@
-export function Album(params){
-    this.name = params.name;
-    this.image = params.image[1]["#text"];
-    this.tracks = params.tracks;    
-}
--- Frontend/JavaScript/MusicApp/entities/artist.js
+++
@@ -1,9 +0,0 @@
-export function Artist(params) {
-    this.name = params.name;
-    this.image = params.image[4]["#text"];
-    this.similar = params.similar;
-    this.bio = params.bio;
-    this.date = params.bio.published;
-    this.stats = params.stats;
-    this.tags = params.tags;
-}
